6dfac639f610a1538ecc7c83f4e214c4b5c27c59
wiseandy32 <wiseandy32@gmail.com>
2025-02-06 13:05:59 +0100

    take site offline

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -11,18 +11,18 @@ export default function NotFoundError() {
       <div className="m-auto flex h-full w-full flex-col items-center justify-center gap-2">
         <h1 className="text-[7rem] font-bold leading-tight">404</h1>
         <span className="font-medium">Oops! Page Not Found!</span>
-        <p className="text-center text-muted-foreground">
+        {/* <p className="text-center text-muted-foreground">
           It seems like the page you&apos;re looking for does not exist or might
           have been removed.
-        </p>
-        <div className="mt-6 flex gap-4">
+        </p> */}
+        {/* <div className="mt-6 flex gap-4">
           <Button variant="outline" onClick={() => navigate(-1)}>
             Go back
           </Button>
           <Button variant="gooeyLeft" onClick={() => navigate("/")}>
             Back to home
           </Button>
-        </div>
+        </div> */}
       </div>
     </div>
   );
